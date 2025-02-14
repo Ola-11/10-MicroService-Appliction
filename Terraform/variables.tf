215bae8464df6ca235da4e45fef33e9313c77e26
Oladeinde Oluwaseun <90369121+Ola-11@users.noreply.github.com>
2025-02-14 12:28:21 -0500

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -12,7 +12,7 @@ variable "prefix" {
 
 variable "resource_group_name_prefix" {
   type        = string
-  default     = "rg"
+  default     = "RG-App"
   description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
 
