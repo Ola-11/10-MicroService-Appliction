eca55b42386500aa7c42aa2bb5241484be4f1ba4
Oladeinde Oluwaseun <90369121+Ola-11@users.noreply.github.com>
2025-02-14 15:35:01 -0500

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -12,14 +12,14 @@ variable "prefix" {
 
 variable "resource_group_name_prefix" {
   type        = string
-  default     = "RG-App"
+  default     = "RG-WebApp"
   description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
 
 variable "node_count" {
   type        = number
   description = "The initial quantity of nodes for the node pool."
-  default     = 3
+  default     = 1
 }
 
 variable "msi_id" {
